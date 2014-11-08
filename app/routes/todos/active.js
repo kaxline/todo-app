1cbb5366137579dd978ee069cd035743f3bb24ae
Keith Axline <kaxline@gmail.com>
2014-11-08 12:54:01 -0800

    app completed
    barring any errors i’m not seeing.

--- app/routes/todos/active.js
+++ app/routes/todos/active.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function () {
     return this.store.filter('todo', function (todo) {
       return !todo.get('isCompleted');
-    })
+    });
   },
 
   renderTemplate: function (controller) {
